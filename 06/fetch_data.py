03a0eaac4b43cf57d7ba5873193869d3b6294682
kubala1210 <jakubgromala@wp.pl>
2025-03-25 14:42:13 +0100

    Zadanie 6

--- 06/fetch_data.py
+++ 06/fetch_data.py
@@ -1,5 +1,17 @@
 import requests
 
 def fetch_data(url):
-    # Uzupełnij kod, aby pobrać dane z URL i obsłużyć ewentualny błąd
-    return ____________
+    response = requests.get(url)
+    if response.status_code == 200:
+        print('Dane pobrane poprawnie')
+        return response.text
+    else:
+        print('Błąd pobierania danych')
+        pass
+   
+
+weather_url = "https://wttr.in/?format=3"
+json_placeholder = "https://jsonplaceholder.typicode.com/posts/1"
+
+print(fetch_data(weather_url))
+print(fetch_data(json_placeholder))
