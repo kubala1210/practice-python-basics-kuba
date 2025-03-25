0fe55a85c4095e32411d55b70eca48d55f0bf325
kubala1210 <jakubgromala@wp.pl>
2025-03-25 14:31:09 +0100

    Zadanie 5

--- 05/square_numbers.py
+++ 05/square_numbers.py
@@ -1,5 +1,5 @@
 def square_numbers(numbers):
-    result = map(lambda x:x**x, numbers)
+    result = list(map(lambda x:x ** 2, numbers))
     return result
 
-print(square_numbers([1, 2, 3, 4])) 
+print(square_numbers([1, 2, 3, 4]))
