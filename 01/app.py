6d5cd2c6a79667723f66238d4521043b47876aae
kubala1210 <jakubgromala@wp.pl>
2025-03-28 09:22:55 +0100

    Poprawione Zadanie 1

--- 01/app.py
+++ 01/app.py
@@ -2,7 +2,7 @@ number = 10
 
 if number > 15:
     print("Liczba jest większa niż 15")
-elif number == 10:
-    print("Liczba jest równa 10")
-else:
-    print("Liczba jest mniejsza niz 10")
+elif number == 15:
+    print("Liczba jest równa 15")
+else: 
+    print("liczba jest mniejsza niż 15")
