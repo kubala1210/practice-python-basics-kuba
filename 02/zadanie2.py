470688bd5a8fe6e00933affdc352c95b94f9b9bd
kubala1210 <jakubgromala@wp.pl>
2025-03-28 09:44:02 +0100

    Update zadanie2.py

--- 02/zadanie2.py
+++ 02/zadanie2.py
@@ -5,11 +5,3 @@ for x in range(1, 21):
         print(f"Liczba {x} jest podzielna przez 3")
     # else:
     #     print(f"Liczba {x} nie jest podzielna przez 3")
-
-
-def square_numbers(numbers):
-    result = list(map(lambda x: x ** 2, numbers))
-    return result
-
-
-print(square_numbers([1, 2, 3, 4]))
