609b03ebfc87bd55a278490603134f0d2d176294
kubala1210 <jakubgromala@wp.pl>
2025-03-28 09:41:47 +0100

    Poprawa zadania 4

--- 04/analyze_list.py
+++ 04/analyze_list.py
@@ -2,4 +2,4 @@ def analyze_list(numbers):
     length = len(numbers)
     smallest = min(numbers)
     largest = max(numbers)
-    return length, smallest, largest
+    return (length, smallest, largest)
